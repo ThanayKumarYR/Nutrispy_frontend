ca57c7a719d3e51f9fb548013046ec8b34eb2f5b
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-16 16:00:20 +0530

    deploy: changes in base name

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Nutrispy_frontend">
       <App />
     </BrowserRouter>
   </React.StrictMode>
