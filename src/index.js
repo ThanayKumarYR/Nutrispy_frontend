3887a62cfed23278fffa0988391501eb094e1711
sujansuja <sujansuja543@gmail.com>
2024-04-30 00:54:03 +0530

    Food template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Appli from "./components/app/Appli";
 
 
